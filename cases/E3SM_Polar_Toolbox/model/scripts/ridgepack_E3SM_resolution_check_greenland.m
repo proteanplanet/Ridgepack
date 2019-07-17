bfd8c5096628f0a3ae61cc973db82a3d89efa049
Andrew Roberts <afroberts@lanl.edu>
2019-07-17 09:19:22 -0600

    addition of E3SM greenland analysis

---
+++ cases/E3SM_Polar_Toolbox/model/scripts/ridgepack_E3SM_resolution_check_greenland.m
@@ -0,0 +1,54 @@
+cont=[5:5:65];
+coastname='Greenland'; % grid name
+
+centlat=-40; % degrees north
+centlon=180; % degrees east
+horizon=10; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=0; % degrees north
+centlon=180; % degrees east
+horizon=5; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=40; % degrees north
+centlon=180; % degrees east
+horizon=10; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=-40; % degrees north
+centlon=-25; % degrees east
+horizon=10; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=0; % degrees north
+centlon=-25; % degrees east
+horizon=5; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=28; % degrees north
+centlon=-72; % degrees east
+horizon=5; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=90; % degrees north
+centlon=-45; % degrees east
+horizon=5; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=58; % degrees north
+centlon=-53; % degrees east
+horizon=2; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=25; % degrees north
+centlon=-80; % degrees east
+horizon=10; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+centlat=70; % degrees north
+centlon=-166; % degrees east
+horizon=5; % degrees of satellite horizon (0-90)
+ridgepack_E3SM_resolution_check(centlat,centlon,horizon,coastname,cont)
+
+
