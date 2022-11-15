c0ae1136d32d34549b7134c2aadb5ba558161001
Andrew Roberts <afroberts@lanl.gov>
2022-11-14 21:36:44 -0700

    Update to meshtest

--- cases/E3SM_Polar_Toolbox/scripts/ridgepack_meshtest.m
+++ cases/E3SM_Polar_Toolbox/scripts/ridgepack_meshtest.m
@@ -2,19 +2,20 @@
 clf
 clear
 
+% set plot configuration
 centlat=65; % degrees north
 centlon=-40; % degrees east
 horizon=8; % degrees of satellite horizon (0-90)
 altitude=1; % Mean Earth radius multiple
 
-% plot location
+% set plot location
 plotloc='/Users/afroberts/Documents';
 
-% grid location
+% set MPAS restart file location
 gridloc=['/Users/afroberts/data/MODEL/E3SM/WC14/r03'];
 gridfile='initial_state.nc';
 
-% obtain grid information
+% obtain grid information from the restart file
 cd(gridloc)
 ncvert=ridgepack_clone(gridfile,{'latVertex','lonVertex',...
                                 'verticesOnCell','indexToCellID',...
@@ -30,6 +31,7 @@ nccell=ridgepack_clone(gridfile,{'latCell','lonCell',...
 
 ncedge=ridgepack_clone(gridfile,{'latEdge','lonEdge'});
 
+% add necessary information to cell array
 ncvert.latCell=nccell.latitude;
 ncvert.lonCell=nccell.longitude;
 ncvert.latEdge=ncedge.latitude;
@@ -41,9 +43,12 @@ ncvert.lonVertex=ncvert.longitude;
 %mkdir(plotloc)
 cd(plotloc)
 
+% plot satellite horizon
 ridgepack_satview(centlat,centlon,horizon)
 
+% plot MPAS mesh
 ridgepack_e3smsatmeshs(ncvert,centlat,centlon,horizon,altitude);
 
+% print the figure
 ridgepack_fprint('png','E3SM_v2_mesh_plot_test',1,2)
 
