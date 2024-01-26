27fea17de4e041763f1d190859b4049f91a33aef
Andrew Roberts <afroberts@lanl.edu>
2024-01-25 17:29:57 -0700

    Additional mesh chekcing

---
+++ cases/E3SM_Polar_Toolbox/scripts/ridgepack_E3SM_gridedge.m
@@ -0,0 +1,190 @@
+
+clf
+clear
+
+fileg='CUSP14'
+
+tesselation=true;
+%tesselation=false;
+
+if tesselation
+ plotchoice=[1:7]
+else
+ plotchoice=[3 5]
+end
+
+plotchoice=5;
+
+for setting=plotchoice
+
+clf
+
+if setting==1
+
+ % Arctic Center
+ centlat=80; % degrees north
+ centlon=-100; % degrees east
+ horizon=60; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ satlat=72; % degrees north
+ satlon=-102; % degrees east
+ sathorizon=20; % degrees of satellite horizon (0-90)
+ titled='Central Arctic';
+
+elseif setting==2
+
+ % Arctic Center
+ centlat=75; % degrees north
+ centlon=-102; % degrees east
+ horizon=20; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ satlat=72; % degrees north
+ satlon=-102; % degrees east
+ sathorizon=8; % degrees of satellite horizon (0-90)
+ titled='Canadian Archipelago';
+
+elseif setting==3
+
+ % Arctic Center
+ centlat=72; % degrees north
+ centlon=-102; % degrees east
+ horizon=8; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ titled='Northwest Passage';
+
+elseif setting==4
+
+ centlat=0; % degrees north
+ centlon=-30; % degrees east
+ horizon=60; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ titled='Atlantic';
+
+elseif setting==5
+
+ % Arctic Center
+ centlat=60; % degrees north
+ centlon=-180; % degrees east
+ horizon=10; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ titled='North Pacific';
+
+elseif setting==6
+
+ % Arctic Center
+ centlat=-90; % degrees north
+ centlon=0; % degrees east
+ horizon=60; % degrees of satellite horizon (0-90)
+ altitude=1; % Mean Earth radius multiple
+ titled='Antarctic';
+
+end
+
+% define contours
+cont=[5 10:1:31];
+ref=20;
+
+% plot location
+plotloc='/Users/afroberts/work';
+
+% grid location
+gridloc=['/Users/afroberts/data/MODEL/E3SM/CUSP/',fileg];
+gridfile='initial_state.nc';
+coastname='CUSP Version 2 14km'; % grid name
+
+% obtain grid information
+cd(gridloc)
+ncvert=ridgepack_clone(gridfile,{'latVertex','lonVertex',...
+                                'verticesOnCell','indexToCellID',...
+                                'nEdgesOnCell','edgesOnCell',...
+                                'cellsOnEdge','cellsOnVertex',...
+                                'edgesOnVertex','bottomDepth'});
+
+nccell=ridgepack_clone(gridfile,{'latCell','lonCell',...
+                                'verticesOnCell','indexToCellID',...
+                                'nEdgesOnCell','edgesOnCell',...
+                                'cellsOnEdge','cellsOnVertex',...
+                                'edgesOnVertex','bottomDepth'});
+
+ncedge=ridgepack_clone(gridfile,{'latEdge','lonEdge'});
+
+ncvert.latCell=nccell.latitude;
+ncvert.lonCell=nccell.longitude;
+ncvert.latEdge=ncedge.latitude;
+ncvert.lonEdge=ncedge.longitude;
+ncvert.latVertex=ncvert.latitude;
+ncvert.lonVertex=ncvert.longitude;
+
+nccell.latVertex=ncvert.latitude;
+nccell.lonVertex=ncvert.longitude;
+nccell.latEdge=ncedge.latitude;
+nccell.lonEdge=ncedge.longitude;
+nccell.latCell=nccell.latitude;
+nccell.lonCell=nccell.longitude;
+
+
+cd(plotloc)
+
+if setting<7
+
+ ridgepack_satview(centlat,centlon,horizon)
+
+ if tesselation
+
+  ridgepack_e3smsatmeshs(ncvert,centlat,centlon,horizon,altitude);
+
+ else
+
+  ridgepack_e3smsatmeshv(nccell,centlat,centlon,horizon,altitude);
+
+ end
+
+ if setting==2 | setting==3
+
+   ridgepack_sathorizon(centlat,centlon,horizon,...
+                        satlat,satlon,sathorizon,[0.83 0.5 0])
+
+ end
+
+elseif setting==7
+
+ ridgepack_polarm('centralarctic','grid','label','noland')
+
+ ridgepack_e3smeshs(ncvert)
+
+ ridgepack_e3smcoastm(ncvert)
+
+ titled='Arctic Ocean';
+
+end
+
+if strcmp(fileg,'CUSP14')
+ title([titled,' CUSP 14-60km',])
+else
+ title([titled,' CUSP 12-60km',])
+end
+
+
+if tesselation
+
+ ridgepack_fprint('png',[fileg,'_tesselation_',num2str(setting)],1,2)
+
+else
+
+ ridgepack_fprint('png',[fileg,'_triangulation_',num2str(setting)],1,2)
+
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ library/graphics/ridgepack_e3smsatmeshspecial.m
@@ -0,0 +1,140 @@
+function [cells]=ridgepack_e3smsatmeshspecial(ncvert,centlat,centlon,...
+                                        horizon,altitude,ncmask,var)
+
+% ridgepack_e3smsatmeshspecial - plot a tesselated E3SM MPAS mesh on the globe
+%
+% function [cells]=ridgepack_e3smsatmeshspecial(ncvert,centlat,centlon,...
+%                                         horizon,altitude,ncmask,var)
+%
+% This function plots the tesselation of an MPAS E3SM mesh on the globe
+% for a chosen horizon specified in degrees from tthe center latitude
+% and longitude of the center of the plot.
+% 
+% INPUT:
+%
+% ncvert   - A netcdf structure containing all of the necessesary grid
+%            information from MPAS
+% centlat  - The central latitude of the plot
+% centlon  - The central longitude of the plot
+% horizon  - The reach of the plot, expressed as a polar angle from the
+%            central latitude and longitude (degrees)
+% altitude - Altitude of the points to be plotted on the sphere
+% ncmask   - Cell indices of cells to be plotted [optional]
+% var      - variable in ncmask used for masking [optional]
+%
+% OUTPUT:
+%
+% cells - indices plotted within the horizon of the figure
+%
+% Ridgepack Version 2.0
+% Andrew Roberts, LANL, 2020 (afroberts@lanl.gov) 
+
+% apply universal mask if non supplied
+if nargin<6
+ ncmask=ncvert;
+ var='nCells';
+end
+
+% height of grid superimposed on plot
+gridheight=1.01; 
+
+% grid color
+gridcolor=[0 0.301 0.435];
+
+% reduce the data use to the plotting area to speed things up
+% and find plotting edge limit of cells
+maxth=deg2rad(horizon);
+for i=1:length(ncvert.nCells.data)
+
+ maxidx=ncvert.nEdgesOnCell.data(i);
+
+ la=ncvert.latVertex.data(ncvert.verticesOnCell.data(1:maxidx,i));
+ lo=ncvert.lonVertex.data(ncvert.verticesOnCell.data(1:maxidx,i));
+
+ [x,y,z,ph,th]=ridgepack_satfwd(rad2deg(la),rad2deg(lo),...
+                                centlat,centlon,horizon,altitude);
+
+ % filter cells not in frame, and find cropping limit
+ if all(isnan(x)) 
+  ncmask.(var).data(i)=NaN;
+ elseif any(isnan(x)) & ~all(isnan(x))
+  [x,y,z,ph,th]=ridgepack_satfwd(rad2deg(la),rad2deg(lo),...
+                     centlat,centlon,horizon,altitude,false);
+  maxt=max(th(:));
+  maxth=max(maxth,maxt);
+ end
+
+end
+
+% allocate cell indices included in the horizon
+cells=find(~isnan(ncmask.(var).data));
+if nargout==1
+ disp('Outputing cells within the satellite view')
+ return
+end
+
+% now draw the grid
+if length(cells)>0
+
+ % Mesh triangulation
+ SHA=ridgepack_e3smeshs(ncvert,cells);
+
+ % Find values within twice the horizon since we've already weeded
+ % out cells that only fall within the horizon so as to plot entire 
+ % cells, not just the vertices within the horizon.
+ [dx,dy,dz,phi,theta]=ridgepack_satfwd([SHA.Latitude],...
+                                       [SHA.Longitude],...
+                                       centlat,centlon,2*horizon,1);
+
+ size(dx)
+
+ dx
+
+ % draw grid
+ plot3(dx,dy,dz,'Color',gridcolor,'LineWidth',0.2)
+ hold on
+
+end
+
+% crop by overlaying a white ring
+N = 100;
+thetavec = linspace(deg2rad(horizon),maxth,N);
+phivec = linspace(0,2*pi,N);
+[th, ph] = meshgrid(thetavec,phivec);
+R = ones(size(th)); % should be your R(theta,phi) surface in general
+cx = R.*sin(th).*cos(ph);
+cy = R.*sin(th).*sin(ph);
+cz = gridheight*R.*cos(th);
+c1 = ones(size(cx));
+clear cc
+cc(:,:,1)=c1;
+cc(:,:,2)=c1;
+cc(:,:,3)=c1;
+surf(cx,cy,cz,cc,'EdgeColor','none');
+
+% add black frame
+ph=deg2rad([0:0.001:361]);
+th=deg2rad(horizon*ones(size(ph)));
+R = ones(size(th)); % should be your R(theta,phi) surface in general
+cx = R.*sin(th).*cos(ph);
+cy = R.*sin(th).*sin(ph);
+cz = gridheight*R.*cos(th);
+plot3(cx,cy,cz,'k')
+
+% make axes equal and tight, set viewing angle
+axis equal
+view([0 0 0.4])
+axis tight
+axis off
+
+% add lighting from infinite sources directly overhead
+if horizon>30
+ light('Position',[0 0 10000],'Style','local')
+ material dull
+end
+
+if nargout<1
+ clear cells
+end
+
+
